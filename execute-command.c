905f9ba600896e7fed2f93011c4e83881000a204
Alan Kha <akhahaha@gmail.com>
2014-01-11 16:01:19 -0800

    Test Compilation Fix
    Added lines to make use of parameters in execute-command.c to allow
    hopefully allow test compilation.

--- execute-command.c
+++ execute-command.c
@@ -20,5 +20,7 @@ execute_command (command_t c, int time_travel)
   /* FIXME: Replace this with your implementation.  You may need to
      add auxiliary functions and otherwise modify the source code.
      You can also use external functions defined in the GNU C Library.  */
+  command_t c_use = c;
+  int t_use = time_travel;
   error (1, 0, "command execution not yet implemented");
 }
