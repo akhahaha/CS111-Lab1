ae3d02953af18cb14e8f917fdd0237586516421e
Alan Kha <akhahaha@gmail.com>
2014-01-16 22:52:15 -0800

    Distributable 1a
    Embedded stack implementation into read-command.c.

--- read-command.c
+++ read-command.c
@@ -15,7 +15,6 @@
 #include "alloc.h"
 #include "command.h"
 #include "command-internals.h"
-#include "stack.h"
 
 #include <ctype.h>		// required to perform isalnum()
 #include <error.h>
@@ -24,12 +23,61 @@
 #include <stdlib.h>		// required to free memory
 #include <string.h>		// required for strchr()
 
+/*-----------------------------------------------------------------------------
+	FILE:   stack.h
+	DESCR:  Simple stack implementation repurposed for making command trees
+	
+	NOTES:	Max items is currently 4 (should not need more for this purpose)
+			Remember to set num_items on initialization.
+			
+	SOURCE:	"Notes from C++ Introduction" (1997) by Jonathan Levene @ MIT
+			http://groups.csail.mit.edu/graphics/classes/6.837/F04/cpp_notes/
+			c++_notes.html
+-----------------------------------------------------------------------------*/
+typedef struct stack stack;
+struct stack
+{
+	command_t commands[4];
+	int num_items;
+};
+
+command_t peek (stack *s)
+{
+	return s->commands[s->num_items - 1];
+}
+
+command_t pop (stack *s)
+{
+	s->num_items--;
+	return s->commands[s->num_items];
+}
+
+command_t push (stack *s, command_t cmd)
+{
+	s->commands[s->num_items] = cmd;
+	s->num_items++;
+	
+	return cmd;
+}
+
+int size (stack *s)
+{
+	return s->num_items;
+}
+
+int is_empty (stack *s)
+{
+	return s->num_items == 0;
+}
+// end stack definitions
+
 // Linked list of command(tree)s
 struct command_stream
 {
 	command_t comm;
 	command_stream_t next;
 };
+
 
 // Enumerated token types
 enum token_type
