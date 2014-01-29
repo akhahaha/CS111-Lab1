19c7e4c14b0d5f8e8673a2ddd631127569f2ca97
Braden Anderson <bradencanderson@gmail.com>
2014-01-28 17:48:07 -0800

    preliminary AND and OR support

--- execute-command.c
+++ execute-command.c
@@ -20,6 +20,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <stdio.h> // printf
 
 // Returns the status of command c.
 int command_status (command_t c)
@@ -28,6 +29,7 @@ int command_status (command_t c)
 }
 
 // Executes command c. No idea what the time_travel arg is for.
+// main() calls this with root. successive recursive calls traverse the tree.
 void execute_command (command_t c, int time_travel)
 {
 	pid_t child;
@@ -57,7 +59,30 @@ void execute_command (command_t c, int time_travel)
 				error(1, 0, "Failed to create child process.");
 
 			break;
+		case AND_COMMAND:
+			c->status = 1;
+			execute_command(c->u.command[0], time_travel);
 
+			if(! c->u.command[0]->status) {	
+				execute_command(c->u.command[1], time_travel);
+				if(! c->u.command[1]->status) {	
+					c->status = 0;
+				}
+			}
+			break;
+		case OR_COMMAND:
+			c->status = 1;
+			execute_command(c->u.command[0], time_travel);
+
+			if(c->u.command[0]->status) {	
+				execute_command(c->u.command[1], time_travel);
+				if(! c->u.command[1]->status) {	
+					c->status = 0;
+				}
+			}
+			else
+				c->status = 0;
+			break;
 		default:
 			break;
 	}
